2aa552365af9b074d9d9e14c6755a8676a86188b
Alex Edelstein <aedelstein@salesforce.comm>
2019-08-23 09:44:26 -0700

    initial commit. importing Enreeco's Apex Formula Evaluator code from https://github.com/enreeco/apex-custom-formula https://blog.enree.co/2015/08/salesforce-apex-formula-help-me-testing.html

---
+++ .sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,8 @@
+declare module "@salesforce/resourceUrl/ApexFormulaSite_Icon" {
+    var ApexFormulaSite_Icon: string;
+    export default ApexFormulaSite_Icon;
+}
+declare module "@salesforce/resourceUrl/SiteSamples" {
+    var SiteSamples: string;
+    export default SiteSamples;
+}
