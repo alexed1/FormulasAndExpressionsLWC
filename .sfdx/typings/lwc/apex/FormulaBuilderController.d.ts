63f1af3864b84770d82d687071bdab567b8db188
Aliaksandr Antoniuk <aleksandrantonyuk89@gmail.com>
2019-08-28 19:25:12 +0300

    Updated logic

--- .sfdx/typings/lwc/apex/FormulaBuilderController.d.ts
+++ .sfdx/typings/lwc/apex/FormulaBuilderController.d.ts
@@ -1,6 +1,3 @@
-declare module "@salesforce/apex/FormulaBuilderController.getFieldList" {
-  export default function getFieldList(param: {objectName: any}): Promise<any>;
-}
 declare module "@salesforce/apex/FormulaBuilderController.parseFormula" {
-  export default function parseFormula(param: {formula: any, recordId: any}): Promise<any>;
+  export default function parseFormula(param: {formula: any, context: any}): Promise<any>;
 }
--- force-app/main/default/lwc/formulaBuilder/formulaBuilder.js
+++ force-app/main/default/lwc/formulaBuilder/formulaBuilder.js
@@ -1,43 +1,60 @@
-import { LightningElement, track, wire } from 'lwc';
-import getFieldList from '@salesforce/apex/FormulaBuilderController.getFieldList';
+import { LightningElement, track } from 'lwc';
 import parseFormula from '@salesforce/apex/FormulaBuilderController.parseFormula';
 
 export default class FormulaBuilder extends LightningElement {
-    @track formula = '';
-    @track parsedFormeula = '';
 
-    @track supportedOperators = [
-        { label: '+', value: '+' },
-        { label: '-', value: '-' },
-        { label: '/', value: '/' },
-        { label: '*', value: '*' },
-        { label: '==', value: '==' },
-        { label: '>=', value: '>=' },
-        { label: '<=', value: '<=' },
-        { label: '<', value: '<' },
-        { label: '>', value: '>' }
-    ];
+    connectedCallback() {
+        let fields = [];
+        let functions = [];
+        let operators = [];
 
-    @track supportedFunctions = [
-        { label: 'COS', value: 'COS' },
-        { label: 'SIN', value: 'SIN' }
-    ]
+        this.contextVariables.forEach(variable => {
+            if (variable.Fields.length > 0) {
+                let objectName = variable.Name;
+                variable.Fields.forEach(field => {
+                    let fieldValue = '$' + objectName + '.' + field;
+                    fields.push({ label: field, value: fieldValue });
+                })
+            } else {
+                let fieldValue = '$' + variable.Name;
+                fields.push({ label: variable.Name, value: fieldValue });
+            }
+        })
 
-    @track contextVariables = [];
+        this.fields = fields;
 
-    @track objectName = 'Opportunity';
-    @track recordId = '0062v00001Fd64sAAB';
+        this.supportedFunctions.forEach(func => {
+            functions.push({ label: func, value: func });
+        })
 
-    @wire(getFieldList, { objectName: '$objectName' })
-    wiredFields({ data }) {
-        if (data) {
-            let fields = [];
-            data.forEach(item => {
-                fields.push({ label: item, value: item });
-            })
-            this.contextVariables = fields;
-        }
+        this.functions = functions;
+        
+        this.supportedOperators.forEach(operator => {
+            operators.push({ label: operator, value: operator });
+        })
+
+        this.operators = operators;
     }
+
+    
+    @track fields;
+    @track functions;
+    @track operators;
+
+    @track formula = '';
+    @track parsedFormula = '';
+
+    @track supportedOperators = ['+', '-', '/', '*', '==', '!=', '>', '<', '>=', '<='];
+    @track supportedFunctions = ['COS', 'SIN'];
+    @track contextVariables = [
+        {"Name" : "foo", "Value" : "555", "Fields" : []}, 
+        {"Name" : "bar", "Value" : "34", "Fields" : []}, 
+        {"Name" : "Opportunity", "Value" : "0062v00001Fd64cAAB", "Fields" : ["Id", "Name", "CreatedDate", "LastModifiedDate"]},
+        {"Name" : "User", "Value" : "", "Fields" : ["Country", "IsActive"]},
+        {"Name" : "Organization", "Value" : "", "Fields" : ["Name", "Country"]},
+        {"Name" : "Profile", "Value" : "", "Fields" : ["CreatedDate", "Name"]},
+        {"Name" : "Setup", "Value" : "", "Fields" : ["PackageSettings__c.Expiration__c", "HierarchyCS__c.Value__c", "HierarchyCS__c.ValueNumber__c"]}
+    ];
 
     selectOperator(event) {
         if (event.detail.value !== '') {
@@ -52,15 +69,15 @@ export default class FormulaBuilder extends LightningElement {
 
     selectField(event) {
         if (event.detail.value !== '') {
-            this.formula = this.formula + '$' + this.objectName + '.' + event.detail.value;
+            this.formula = this.formula + event.detail.value + ' ';
             event.target.value = '';
         }
     }
 
     parsedQuery() {
-        parseFormula({ formula: this.formula, recordId: this.recordId })
+        parseFormula({ formula: this.formula, context: JSON.stringify(this.contextVariables) })
             .then(result => {
-                this.parsedFormeula = result;
+                this.parsedFormula = result;
             })
     }
 }
